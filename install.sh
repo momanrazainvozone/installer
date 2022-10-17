b09b73f4e802c6bd8d5c7365dc6066606ddc1c43
moman raza <moman.raza@invozone.com>
2022-10-17 21:46:32 +0500

    node js path issue

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 echo "** Installing Project dependency, It may take some time. **" &&
 echo "** Installing Node JS**" &&
-sudo cp -r node-v16.18.0-linux-x64/{bin,include,lib,share} /usr/ &&
+sudo cp -r node/{bin,include,lib,share} /usr/ &&
 export PATH=/usr/node-v16.18.0-linux-x64/bin:$PATH
 echo "**  installing pm2 **" &&
 sudo npm i ./pm2-master
@@ -9,5 +9,4 @@ echo "** Installing redis server **" &&
 cd redis-stable && make && sudo install make && cd ..
 echo "** Installing postgress SQL Server **"
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list' && 
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add - && 
-sudo apt-get -y install postgresql
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
