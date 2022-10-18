1a3c34419e19d52bf83531057e6e947e28f7424b
moman raza <moman.raza@invozone.com>
2022-10-18 20:03:11 +0500

    fix issue postgress

--- install.sh
+++ install.sh
@@ -6,24 +6,23 @@ export PATH=/usr/node-v16.18.0-linux-x64/bin:$PATH
 echo "**  installing pm2 **" &&
 sudo npm i ./pm2-master -g &&
 echo "** ======================================  Preparing postgress SQL Depenency  ====================================== **" &&
+
 cd postgresql &&
 ./configure &&
 sudo make &&
 echo "** ======================================  Installing postgress SQL Server ====================================== **" &&
 
-sudo su -c  'make install' && 
+sudo su -c  'make install' root && 
 echo "** ======================================  Adding user postgres ====================================== **" &&
-
-sudo su -c  'adduser postgres || echo "User already exists."' && 
-sudo su -c  'rm -rf  /usr/local/pgsql/data' &&
+sudo su -c  'adduser postgres' root && 
 echo "** ======================================  Clear Data ====================================== **" &&
 
-sudo su -c 'mkdir /usr/local/pgsql/data' && 
-sudo su -c  'chown postgres /usr/local/pgsql/data' && 
+sudo su -c 'mkdir /usr/local/pgsql/data' root && 
+sudo su -c  'chown postgres /usr/local/pgsql/data' root && 
 echo "** ======================================  Finalizing ====================================== **" &&
 
 sudo su -c   '/usr/local/pgsql/bin/initdb -D /usr/local/pgsql/data' postgres && 
-sudo su -c   '/usr/local/pgsql/bin/postgres -D /usr/local/pgsql/data >logfile 2>&1 &' postgres  && 
+sudo su -c   '/usr/local/pgsql/bin/pg_ctl -D /usr/local/pgsql/data -l logfile start' postgres  && 
 sudo su -c    '/usr/local/pgsql/bin/createdb test' postgres &&
 exit && exit && export PATH=/usr/local/pgsql/bin:$PATH &&
 cd .. &&
