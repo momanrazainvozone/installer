f32c8772eab33819bf218cd08bc54fc77df81184
moman raza <moman.raza@invozone.com>
2022-10-21 18:16:12 +0500

    skip postgress installation

--- install.sh
+++ install.sh
@@ -11,19 +11,19 @@ echo "** ======================================  Preparing postgress SQL Depenen
 cd postgresql &&
 ./configure &&
 sudo make &&
-echo "** ======================================  Installing postgress SQL Server ====================================== **" &&
+# echo "** ======================================  Installing postgress SQL Server ====================================== **" &&
 
-sudo su -c  'make install' root && 
-echo "** ======================================  Adding user postgres ====================================== **" &&
-sudo su -c  'adduser postgres' root && 
-sudo su -c 'mkdir /usr/local/pgsql/data' root && 
-sudo su -c  'chown postgres /usr/local/pgsql/data' root && 
-echo "** ======================================  Finalizing ====================================== **" &&
+# sudo su -c  'make install' root && 
+# echo "** ======================================  Adding user postgres ====================================== **" &&
+# sudo su -c  'adduser postgres' root && 
+# sudo su -c 'mkdir /usr/local/pgsql/data' root && 
+# sudo su -c  'chown postgres /usr/local/pgsql/data' root && 
+# echo "** ======================================  Finalizing ====================================== **" &&
 
-sudo su -c '/usr/local/pgsql/bin/initdb -D /usr/local/pgsql/data' postgres && 
-sudo su - postgres -c ' /usr/local/pgsql/bin/pg_ctl -D /usr/local/pgsql/data -l logfile start'   && 
-sudo su -c '/usr/local/pgsql/bin/createdb test' postgres &&
-export PATH="$PATH:/usr/local/pgsql/bin" &&
+# sudo su -c '/usr/local/pgsql/bin/initdb -D /usr/local/pgsql/data' postgres && 
+# sudo su - postgres -c ' /usr/local/pgsql/bin/pg_ctl -D /usr/local/pgsql/data -l logfile start'   && 
+# sudo su -c '/usr/local/pgsql/bin/createdb test' postgres &&
+# export PATH="$PATH:/usr/local/pgsql/bin" &&
 echo "** Installing redis server **" &&
 cd ../redis &&
 sudo make &&
